17965c6821b0ffc3e896ac666edee5f3895181e3
Paul Hagwood <SubtleKnife@Pauls-MacBook-Pro.local>
2020-10-30 15:17:57 -0400

    First Files Added

---
+++ index.js
@@ -0,0 +1,82 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// array of questions for user
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a short description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'toc',
+        message: 'Please enter a table of contents, each item separated by a comma.'
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Please enter installation instructions.'
+    },
+    {
+        type: 'input',
+        name: 'useage',
+        message: 'How will this project be used?'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license.',
+        choices: [
+            "MIT License",
+            "Apache License",
+            "GPL License",
+            "Other"
+        ]
+    },
+    {
+        type: 'input',
+        name: 'guidelines',
+        message: 'Please enter guidelines for contributing.'
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Please enter any testing instructions.'
+    },
+    {
+        type: 'input',
+        name: 'userid',
+        message: 'Please enter your github user id.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address.'
+    },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("ReadMe was created");
+    })
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
---
+++ testReadMe.md
@@ -0,0 +1,33 @@
+# Title of the Project
+
+## Description of the project
+Insert description here.
+
+## Table of Contents
+- [instructions](#-Installation-Instructions)
+- [How it is used](#-How-the-Project-Should-Be-Used)
+- [License](#-License)
+- [Contribution](#-Contribution-Guidelines)
+- [Testing](#-Test-Instructions)
+- [Questions](#-Questions)
+
+# Installation Instructions
+Insert installation instructions here. 
+
+# How the Project Should Be Used
+Insert usage here.
+
+# License 
+Choose from a list of options.
+
+# Contribution Guidelines
+Insert Contribution guidelines here.
+
+# Test Instructions
+Insert Test instructions here.
+
+# Questions
+[github user name](https://github.com/pmhagwood/) 
+
+[email](https://github.com/pmhagwood/weatherdashboard)
+
